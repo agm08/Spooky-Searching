8aeea51291ffe1aaa5cfdb666117fb871b0f24a4
Al Manrique <56333028+agm08@users.noreply.github.com>
2019-12-02 18:05:29 -0500

    Create multitest.h

---
+++ multitest.h
@@ -0,0 +1,21 @@
+  
+#include<stddef.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#ifndef _multitest_h_	//if header is not defined
+#define _multitest_h_	//then it will run this line and define it
+
+
+#ifdef thread
+#define findTarget(target, size, childrenNum, split, list) findTarget_thread(target, size, childrenNum, split, list)
+void* splitSearch(void*);
+int findTarget_thread(int, int, int, int, int*);
+#endif
+
+#ifdef proc
+#define findTarget(target, size, childrenNum, split, list) findTarget_proc(target, size, childrenNum, split, list)
+int findTarget_proc(int, int, int, int, int*);
+#endif
+
+#endif
